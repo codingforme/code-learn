e35586e2ae8f6ed31cf598bdca643ef9e4bf9951
卢丰 <lufeng@lufengdeMacBook-Pro.local>
2018-12-09 13:37:42 +0800

    go demo

---
+++ go/identifier.go
@@ -0,0 +1,18 @@
+package main
+
+var x, y int
+var (  // 这种因式分解关键字的写法一般用于声明全局变量
+    a int
+    b bool
+)
+
+var c, d int = 1, 2
+var e, f = 123, "hello"
+
+//这种不带声明格式的只能在函数体中出现
+//g, h := 123, "hello"
+
+func main(){
+    g, h := 123, "hello"
+    println(x, y, a, b, c, d, e, f, g, h)
+}
